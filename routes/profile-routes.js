d21472d29c921be194b9f58ef4cc33e62d5bd717
bhaveshgit <bhavesh089git@gmail.com>
2020-08-20 18:43:53 +0530

    .

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const lazadaUser = require('../models/lazadaUser-model');
+
 const authCheck = (req, res, next) => {
 	if (!req.user) {
 		// if user not logged in
@@ -22,8 +24,12 @@ const authCheck = (req, res, next) => {
 // 		next();
 // 	}
 // };
+const lazadaUsercheck = (req, res, next) => {
+	res.locals.lazadause = lazadaUser.findOne({ userID: req.user.id }).exec();
+	return next();
+};
 
-router.get('/', authCheck, (req, res, next) => {
+router.get('/', authCheck, lazadaUsercheck, (req, res, next) => {
 	//res.send("Greetings, " + req.user.username + "! you are logged in. ");
 	// let lazadaUser = req.session.result;
 	// console.log(res.locals.lazadauser);
@@ -31,7 +37,7 @@ router.get('/', authCheck, (req, res, next) => {
 	// console.log(JSON.parse(lazada));
 
 	console.log(req.session.lazadUser);
-	res.render('profile', { user: req.user, lazadauser: req.session.lazadaUsers });
+	res.render('profile', { user: req.user, lazadauser: res.locals.lazadause });
 });
 
 module.exports = router;
