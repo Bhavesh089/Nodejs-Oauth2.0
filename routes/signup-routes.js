285f97b494d60fa3de33223ab19786b9c2031a02
bhaveshgit <bhavesh089git@gmail.com>
2020-08-21 17:01:14 +0530

    .

--- routes/signup-routes.js
+++ routes/signup-routes.js
@@ -1,17 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const authCheck = (req, res, next) => {
-	if (!req.user) {
-		// if user not logged in
-		res.redirect('/auth/home');
-	} else {
-		// if logged in
-		next();
-	}
-};
-
-router.get('/', authCheck, (req, res, next) => {
+router.get('/', (req, res, next) => {
 	res.render('signup', { user: req.user });
 });
 
