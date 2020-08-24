20df3703b4a1b60317f53c8784a7d83ecbfcb7dc
bhaveshgit <bhavesh089git@gmail.com>
2020-08-24 22:59:30 +0530

    .

---
+++ config/keys.js
@@ -0,0 +1,21 @@
+module.exports = {
+	google: {
+		clientID: '952648099364-thsmb26rha8ngugu9hd2mnjj4ukdahk7.apps.googleusercontent.com',
+		clientSecret: 'k-cEUGUDheSozyhsqUASTUB7'
+	},
+	facebook: {
+		clientID: '3379943188735842',
+		clientSecret: '4b50185a251cb32fdff27eb3fd4840bb'
+	},
+	lazada: {
+		clientID: '121585',
+		clientSecret: 'dzFd2J1lCBbE3vs2I7l9LsytrP23OroD'
+		//clientID: "121695",
+	},
+	mongodb: {
+		dbURI: 'mongodb+srv://movies:c5JjjpKc2TraWkEO@moviesdb.3zwnc.mongodb.net/shop?retryWrites=true&w=majority'
+	},
+	session: {
+		cookieKey: 'theskyhasfallen'
+	}
+};
