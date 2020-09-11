8f39bfe9c15c3afca95a439493f35167b6a00e26
bhaveshgit <bhavesh089git@gmail.com>
2020-09-11 20:52:05 +0530

    .

--- Middleware/authcheck.js
+++ Middleware/authcheck.js
@@ -1,22 +1,49 @@
 const jwt = require('jsonwebtoken');
 var secret = require('../config/keys');
+let user = require('../models/user-model');
 
+const SocialUsercheck = (req, res, next) => {
+	//checking logged user in lazadaUser model
+	user.findOne({ accessToken: req.user.accessToken }).exec().then((result) => {
+		if (result) {
+			//if user present store it in local and pass next.
+			return true;
+		} else {
+			//if not null
+			return false;
+		}
+	});
+};
 exports.authtokenCheck = (req, res, next) => {
-	// const socialUser = req.user;
-	if (
-		(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') ||
-		(req.user && req.user.accessToken)
-	) {
-		const token = req.headers.authorization.split(' ')[1];
-		jwt.verify(token, secret.Jwt.secret, (err, user) => {
-			if (err) return res.sendStatus(403);
-			req.user = user;
+	if (req.user) {
+		if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+			const token = req.headers.authorization.split(' ')[1];
+			jwt.verify(token, secret.Jwt.secret, (err, user) => {
+				if (err) return res.sendStatus(403);
+				req.user = user;
+				next();
+			});
+		} else if (SocialUsercheck) {
 			next();
-		});
+		} else {
+			return res.sendStatus(401);
+		}
 	} else {
 		return res.sendStatus(401);
 	}
 };
+// exports.authtokenCheck = (req, res, next) => {
+// 	if ((req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') || req.user) {
+// 		const token = req.headers.authorization.split(' ')[1];
+// 		jwt.verify(token, secret.Jwt.secret, (err, user) => {
+// 			if (err) return res.sendStatus(403);
+// 			req.user = user;
+// 			next();
+// 		});
+// 	} else {
+// 		return res.sendStatus(401);
+// 	}
+// };
 
 // var auth = {
 //   required: jwt({
