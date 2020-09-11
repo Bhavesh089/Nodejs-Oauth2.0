11ff771e741061293155cb544ac0e1874151a493
bhaveshgit <bhavesh089git@gmail.com>
2020-09-11 21:08:50 +0530

    .

--- Middleware/authcheck.js
+++ Middleware/authcheck.js
@@ -2,18 +2,6 @@ const jwt = require('jsonwebtoken');
 var secret = require('../config/keys');
 let user = require('../models/user-model');
 
-const SocialUsercheck = (req, res, next) => {
-	//checking logged user in lazadaUser model
-	user.findOne({ accessToken: req.user.accessToken }).exec().then((result) => {
-		if (result) {
-			//if user present store it in local and pass next.
-			return true;
-		} else {
-			//if not null
-			return false;
-		}
-	});
-};
 exports.authtokenCheck = (req, res, next) => {
 	if (req.user) {
 		if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
